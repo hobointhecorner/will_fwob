591243e4997ff166846ea9d297ddc3d8fb29fdfa
Cody Lape <cody@hobomedia.net>
2021-04-12 19:16:51 -0500

    WOO TERRAFORM YEAH WOO

---
+++ main.tf
@@ -0,0 +1,28 @@
+locals {
+  webpage_root = "files/pages"
+}
+
+resource "aws_s3_bucket" "bucket" {
+  bucket = var.bucket_name
+  acl    = "public-read"
+
+  policy = templatefile(
+    "${path.module}/files/bucket_policy.json.tmpl",
+    { bucket_name = var.bucket_name }
+  )
+
+  website {
+    index_document = "index.html"
+  }
+}
+
+resource "aws_s3_bucket_object" "pages" {
+  for_each = fileset(path.module, "${local.webpage_root}/**")
+
+  bucket = aws_s3_bucket.bucket.id
+  key    = trimprefix(each.value, "${local.webpage_root}/")
+  source = each.value
+
+  acl          = "public-read"
+  content_type = "text/html"
+}
